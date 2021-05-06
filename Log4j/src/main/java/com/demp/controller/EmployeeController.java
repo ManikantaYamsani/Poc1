c9175fc7d7415780e596c6c63042d458d1b57031
Manikanta Yamsani <manikantayamsani@gmail.com>
2021-05-06 19:27:02 +0530

    log 4j file

---
+++ Log4j/src/main/java/com/demp/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.demp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping("/diplay")
+	public String Show() {
+		logger.trace("This is a TRACE message.");
+		logger.debug("This is a DEBUG message.");
+		logger.info("This is an INFO message.");
+		logger.warn("This is a WARN message.");
+		logger.error("You guessed it, an ERROR message.");
+		return "Cherry java";
+
+	}
+
+}
